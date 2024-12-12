934049a1b3d61ad28919d2cd9dba2511aa762000
Ib64-star <bonnebonneesperance@gmail.com>
2024-12-12 10:36:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['242 068131261']
+global.premium = ['242 068131261']
+global.ownernomer = '242068131261'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'ren -kadji'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
